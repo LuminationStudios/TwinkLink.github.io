d02d7d4b18ad93119de34d7d567adc4679d5aabd
LuminationStudios <studios.lumination@gmail.com>
2025-10-02 16:36:01 -0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        twinkPink: "#f9c5d1",
+        twinkBlue: "#a7d8f5",
+      },
+    },
+  },
+  plugins: [],
+}
