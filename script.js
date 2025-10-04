4ffa7bc76bf30be296a63b0f1cd94d606faf0274
LuminationStudios <studios.lumination@gmail.com>
2025-10-04 14:04:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -49,6 +49,28 @@ fetch("./websites.json")
     }
   });
 
+// Load Websites Projects
+fetch("./roblox.json")
+  .then(response => response.json())
+  .then(data => {
+    const container = document.querySelector("#roblox-grid");
+    if (container) {
+      data.forEach(project => {
+        const card = document.createElement("div");
+        card.classList.add("project-card");
+        card.innerHTML = `
+          <div class="project-image-box">
+            <img src="${project.image}" alt="${project.title}">
+          </div>
+          <h3>${project.title}</h3>
+          <p>${project.description}</p>
+          <a href="project.html?id=${project.id}" class="btn">View Project</a>
+        `;
+        container.appendChild(card);
+      });
+    }
+  });
+
 fetch("./header.json")
   .then(response => response.json())
   .then(data => {
